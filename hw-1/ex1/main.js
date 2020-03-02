ec77385046e207be8fe961d0cfe8250e4903969f
ikurgans <ivan.kurhanskiy@pmi.com>
2020-03-02 23:09:17 +0200

    Update main.js

--- hw-1/ex1/main.js
+++ hw-1/ex1/main.js
@@ -1,7 +1,7 @@
-const min1 = require("./min");
-const sum1 = require("./sum");
-const abs1 = require("./abs");
+const min = require("./min");
+const sum = require("./sum");
+const abs = require("./abs");
 
-console.log(min1([43, -15,-15.000999,0,4,40,12, -16]));
-console.log(sum1(-5.64846, 388));
-console.log(abs1(-45));
+console.log(min([43, -15,-15.000999,0,4,40,12, -16]));
+console.log(sum(-5.64846, 388));
+console.log(abs(-45));
